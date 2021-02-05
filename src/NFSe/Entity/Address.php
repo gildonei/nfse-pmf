e6762760d05bab6c271cab865cd4331048f8a712
gildonei <gildonei.mendes@gmail.com>
2021-02-05 18:31:42 -0300

    Fixed message of invalid state

--- src/NFSe/Entity/Address.php
+++ src/NFSe/Entity/Address.php
@@ -131,7 +131,7 @@ class Address extends AbstractEntity
         }
         $state = strtolower($state);
         if (!in_array($state, ['AC','AL','AM','AP','BA','CE','DF','ES','GO','MA','MG','MS','MT','PA','PB','PE','PI','PR','RJ','RN','RO','RR','RS','SC','SE','SP','TO'])) {
-            throw new \InvalidArgumentException('State is inválid!');
+            throw new \InvalidArgumentException('State is invalid!');
         }
         $this->state = $state;
 
